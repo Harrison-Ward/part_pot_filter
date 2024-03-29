f77d64755c57656372d5b49feb9247d69a58fdd0
Harrison Ward <hward4116@gmail.com>
2024-03-28 23:48:42 -0700

    added notebook to test and tune model params

--- predict_pot.py
+++ predict_pot.py
@@ -23,13 +23,13 @@ win_probability = 0.05
 true_bid_value = true_pot_value * win_probability
 bid_std = 0.10 * true_bid_value
 n_bids = 64
+n_simulations = 10
 
 observed_bids = np.random.normal(loc=true_bid_value, scale=bid_std, size=n_bids)
 
 # model parameters
-std_dev_increase = 1_000  # Assume some increase in prediction uncertainty
-measurement_noise = 1_000
-
+measurement_noise = 2_000
+std_dev_increase = 300
 
 # prediction function to simulate particle evolution
 def predict(particles, std_dev_increase):
@@ -58,31 +58,41 @@ def estimate(particles, weights):
     return np.average(particles, weights=weights, axis=0)
 
 
-estimates = []
-for observed_bid in observed_bids:
-    predict(particles, std_dev_increase)
-    update(particles, weights, observed_bid, win_probability, measurement_noise)
-    resample(particles, weights)
-    pot_estimate = estimate(particles, weights)
-    estimates.append(pot_estimate)
+estimates_dic = {}
+for i in range(n_simulations):
+    print(f'Running simulation {i + 1} of {n_simulations}')
+    estimates = []
+    for observed_bid in (observed_bids):
+        predict(particles, std_dev_increase)
+        update(particles, weights, observed_bid, win_probability, measurement_noise)
+        resample(particles, weights)
+        pot_estimate = estimate(particles, weights)
+        estimates.append(pot_estimate)
+    estimates_dic[i]= estimates
 
-
+print("estimates dict: ", estimates_dic.values())
 # visualize results
 graphs_dir = 'outputs'
 if not os.path.exists(graphs_dir):
     os.makedirs(graphs_dir)
 
+x_plot = np.arange(n_bids)
+
+plt.clf()
 plt.style.use('fivethirtyeight')
-plt.title('Estimation of pot size as a function of bids observed')
+plt.title(f'{n_simulations} estimates of pot size as a function of bids observed')
 plt.ylabel('Pot Size')
 plt.xlabel('i\'th Bid')
-plt.plot(np.arange(n_bids), estimates, label='Pot size estimate')
+for i in range(n_simulations):
+    plt.plot(x_plot, estimates_dic[i], alpha=0.25)
 plt.hlines(y=true_pot_value, xmin=0, xmax=n_bids, colors='gray', label=f'True pot size: ${true_pot_value:,.0f}')
 plt.hlines(y=initial_pot_guess, xmin=0, xmax=n_bids, colors='gray', linestyles='--', label=f'Intial estimate: ${initial_pot_guess:,.0f}')
 plt.legend()
-plt.savefig(f'{graphs_dir}/estimate_time_series.png')
+# plt.savefig(f'{graphs_dir}/estimate_time_series.png')
+plt.show()
 plt.clf()
 
+plt.clf()
 sns.set_style("whitegrid")  # Setting the seaborn style to whitegrid for a cleaner background
 # Create the KDE plot
 sns.kdeplot(particles, bw_adjust=0.5, fill=True, color="skyblue", alpha=0.5)
@@ -101,5 +111,7 @@ ax = plt.gca()  # Get the current Axes instance
 ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'${int(x):,}'))
 
 # Show the plot
-plt.savefig(f'{graphs_dir}/particle_kde.png')
+
+plt.show()
+# plt.savefig(f'{graphs_dir}/particle_kde.png')
 
